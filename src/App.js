b1fe0220898232c8cd140d5427b3b5f853737d01
Alejandro Garcia <agarciacoding@gmail.com>
2023-06-14 18:59:25 -0500

    added response variable/.env file added with token/gitignore .env added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-
 import './App.css';
+
+const getAirQuality = async (city) => {
+  try{                                                          //create-react-app handles env variables so no need to npm i env
+    const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${REACT_APP_AQI_API_TOKEN}`)
+  }catch {
+
+  }
+}
 
 function App() {
   return (
