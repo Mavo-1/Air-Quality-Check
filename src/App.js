3c2c4fb86ff5ff8aca17d701c099b122e98675cc
Alejandro Garcia <agarciacoding@gmail.com>
2023-06-15 13:00:16 -0500

    updated function app to add response

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+import React, {useState} from 'react';
 import './App.css';
 
 
-const getAirQuality = async (city) => {
-  try{                             //create-react-app handles env variables so no need to npm i env
-    const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${REACT_APP_AQI_API_TOKEN}`)
-    const data = await response.json()
-    console.log(data)
-  }catch {
 
-  }
-}
 
 function App() {
-  return (
-    <h1>Hello Universe</h1>
-  );
+  const [airQualityData, setAirQualityData] = useState(null)
+  const [error, setError] = useState(null);
+
+    const getAirQuality = async (city) => {
+      try{                             //create-react-app handles env variables so no need to npm i env
+        const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+        const data = await response.json()
+        console.log(data)
+        if(response.ok && data.status === 'ok'){
+          setAirQualityData(data.data)
+          setError(null)
+        }else {
+          setError("Sorry, we couldn't find the city you were looking for. Try another location or make sure spelling is correct.")
+        }
+      }catch (error) {
+        console.error('network error:', error)
+        //set error state
+        //set air quality data to nu ll
+      }
+    }
+    return (
+      <h1>Hello Alex</h1>
+    );
 }
 
 export default App;
