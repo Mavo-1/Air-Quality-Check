b16cec47cf8f1b29f36d4f198d717aad018ceca0
Alejandro Garcia <agarciacoding@gmail.com>
2023-06-15 11:03:20 -0500

    added const data to response variable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+
 const getAirQuality = async (city) => {
-  try{                                                          //create-react-app handles env variables so no need to npm i env
+  try{                             //create-react-app handles env variables so no need to npm i env
     const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${REACT_APP_AQI_API_TOKEN}`)
+    const data = await response.json()
+    console.log(data)
   }catch {
 
   }
